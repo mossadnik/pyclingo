22087a92f795da3dbb37a7082410e5ba84227a7f
Matthias Ossadnik <ossadnik.matthias@gmail.com>
2025-09-18 11:34:15 +0200

    Cleanup

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -6,7 +6,8 @@ from pyclingo.api import var, lit
 class Test_generate_statement:
     @pytest.mark.parametrize('expr, expected', [
         [var('A') == lit(1), 'A = 1'],
-        [var('A') != lit(1), 'A != 1']
+        [var('A') != lit(1), 'A != 1'],
+        [var('A') == var('B'), 'A = B'],
     ])
     def test_binary_operator(self, expr, expected):
         actual = generate_statement(expr)
